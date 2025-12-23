23ebcaad906e1b8a670c08f0023e802dc6cc3cff
blefmuren <63847797+blefmuren@users.noreply.github.com>
2025-12-24 01:13:16 +0700

    Update RDP_patch.ps1
    добавим таймер для ребута системы, на случай если чтото пойдет не так. Причина: если скрипт отработает не штатно, то можем потерять доступ по РДП до системы.

--- Desktop/RDP_patch.ps1
+++ Desktop/RDP_patch.ps1
@@ -1,7 +1,17 @@
 # PowerShell скрипт модифицирует файл termsrv.dll, разрешая множественные RDP подключения к рабочим станциям на базе Windows 10 (1809 и выше) и Windows 11
 # Подробности https://winitpro.ru/index.php/2015/09/02/neskolko-rdp-sessij-v-windows-10/
 
+# таймер для ребута системы, на случай если чтото пойдет не так. Причина: если скрипт отработает не штатно, то можем потерять доступ по РДП до системы.
+$useTimer = (Read-Host "Start reboot timer? (y/n). The system will automatically reboot if something doesn't work. You can stop the reboot once you've connected.") -match '^(y|yes|д|да)$'
+if ($useTimer) {
+    $m = Read-Host "How many minutes does it take to reboot? (default: 15)"
+    if (-not $m) { $m = 15 }
+    $sec = [int]$m * 60
+    shutdown /r /t $sec | Out-Null
+    $timerStarted = $true
+}
 
+try {
 # Остановить службу, сделать копию файл и изменить разрешения
 Stop-Service UmRdpService -Force
 Stop-Service TermService -Force
@@ -34,5 +44,11 @@ fc.exe /b c:\windows\system32\termsrv.dll.patched c:\windows\system32\termsrv.dl
 # замена оригинального файла
 Copy-Item c:\windows\system32\termsrv.dll.patched c:\windows\system32\termsrv.dll -Force
 Set-Acl c:\windows\system32\termsrv.dll $termsrv_dll_acl
+}
+finally {
 Start-Service UmRdpService
 Start-Service TermService
+if ($timerStarted -and ((Read-Host "Cancel reboot timer? (y/n)") -match '^(y|yes|д|да)$')) {
+        shutdown /a | Out-Null
+    }
+}
